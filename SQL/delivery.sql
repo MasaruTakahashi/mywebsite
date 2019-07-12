c9212971175b39ce6fdc2ab92c21a8e51ee75817
高橋優 <masaru.takahashi.likeit@gmail.com>
2019-07-12 10:14:37 +0900

    初期入力を追加

--- SQL/delivery.sql
+++ SQL/delivery.sql
@@ -3,3 +3,5 @@ CREATE TABLE delivery(
 id SERIAL,
 detail varchar(256)
 );
+INSERT INTO delivery(detail) VALUES('送料込み');
+INSERT INTO delivery(detail) VALUES('着払い');
--- SQL/item_state.sql
+++ SQL/item_state.sql
@@ -3,3 +3,7 @@ CREATE TABLE item_state(
 id SERIAL,
 detail varchar(256)
 );
+INSERT INTO item_state(detail) VALUES('新品');
+INSERT INTO item_state(detail) VALUES('未使用に近い');
+INSERT INTO item_state(detail) VALUES('傷や汚れあり');
+INSERT INTO item_state(detail) VALUES('状態が悪い');
